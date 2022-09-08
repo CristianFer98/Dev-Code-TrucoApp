bbf794a628fced06bb8a919377f7d3e8ca85aaf3
julieta96 <julietaextra14@gmail.com>
2022-09-08 16:11:30 -0300

    acessorios js y css

---
+++ TrucoApp-DevCode/ClientApp/src/components/tienda/Accesorios.js
@@ -0,0 +1,31 @@
+﻿import React from "react";
+import "./accesorios.css";
+const Accesorios = ({imagen , descripcion, precio}) => {
+
+    return (
+        <div className="componente-accesorios">
+            <div className="accesorio-componente">
+                <div className="accesorio-imagen">
+                    <img src={imagen}/>
+                </div>
+                <div className="accesorio-descripcion">
+                    <strong>{descripcion}</strong>
+                </div>
+                <div className="accesorio-precio">
+                    <strong>${precio}</strong>
+                </div>
+                <div className="componente-btn">
+                    <div className="btn-detalles">
+                        VER DETALLES
+                    </div>
+                    <div className="btn-comprar">
+                        COMPRAR
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        );
+}
+
+export default Accesorios;
