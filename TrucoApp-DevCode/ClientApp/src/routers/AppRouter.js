55709bee3afb43d6d8d60428f2566767eb05c1f4
Ivan Kek <ivan.kek@hotmail.com>
2022-09-18 19:06:01 -0300

    Agregando ruta mesas disponibles

--- TrucoApp-DevCode/ClientApp/src/routers/AppRouter.js
+++ TrucoApp-DevCode/ClientApp/src/routers/AppRouter.js
@@ -11,7 +11,8 @@ import { LoginRegistro } from "../components/auth/LoginRegistro";
 import { useSelector } from "react-redux";
 import { PrivateRoute } from "./PrivateRoute";
 import { Truco1vs1 } from "../components/juego/Truco1vs1";
-import {Avatar} from "../components/avatar/Avatar";
+import { Avatar } from "../components/avatar/Avatar";
+import { MesasDisponibles } from "../components/mesas/MesasDisponibles";
 
 export const AppRouter = () => {
   const { uid } = useSelector((state) => state.auth);
@@ -30,6 +31,8 @@ export const AppRouter = () => {
               <Route exact path="/inicio/reglas" component={Reglas} />
               <Route exact path="/inicio/nosotros" component={Nosotros} />
               <Route exact path="/inicio/avatar" component={Avatar} />
+              <Route exact path="/inicio/mesas" component={MesasDisponibles} />
+
               <Redirect to="/inicio" />
             </Layout>
           </Route>
