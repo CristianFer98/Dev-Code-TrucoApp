b24804379bdc4a519971bac18b2f08313abd5d82
julieta96 <julietaextra14@gmail.com>
2022-09-18 18:44:22 -0300

    se puede modificar las cejas

--- TrucoApp-DevCode/ClientApp/src/components/avatar/Avatar.js
+++ TrucoApp-DevCode/ClientApp/src/components/avatar/Avatar.js
@@ -8,11 +8,16 @@ import peloCanoso from "./../../assets/avatar/pelo5.png";
 
 import ropa from "./../../assets/avatar/ropa.png";
 
-const setPelo = (peloNuevo) =>{
+const setPelo = (peloNuevo, cejaColorNuevo) => {
 	let peloActual = document.querySelector("#pelo-actual");
-	peloActual.src=peloNuevo;
+	let cejaColorActual = document.querySelector(".ceja-izq").classList[1];
+	document.querySelector(".ceja-izq").classList.remove(cejaColorActual);
+	document.querySelector(".ceja-der").classList.remove(cejaColorActual);
+	document.querySelector(".ceja-izq").classList.add(cejaColorNuevo);
+	document.querySelector(".ceja-der").classList.add(cejaColorNuevo);
+	peloActual.src = peloNuevo;
 }
-const setPiel = (colorNuevo)=>{
+const setPiel = (colorNuevo) => {
 
 	let colorActual = document.querySelector(".cabeza").classList[1];
 
@@ -29,21 +34,21 @@ const setPiel = (colorNuevo)=>{
 	document.querySelector(".cuello").classList.add(colorNuevo);
 	document.querySelector(".brazo-der").classList.add(colorNuevo);
 	document.querySelector(".brazo-izq").classList.add(colorNuevo);
-		
+
 }
 
-const setOjos = (colorNuevo)=>{
+const setOjos = (colorNuevo) => {
 
-		let colorActual = document.querySelector(".iris-der").classList[1];
+	let colorActual = document.querySelector(".iris-der").classList[1];
 
-	    document.querySelector(".iris-izq").classList.remove(colorActual);
-		document.querySelector(".iris-der").classList.remove(colorActual);
-		document.querySelector(".iris-izq").classList.add(colorNuevo);
-		document.querySelector(".iris-der").classList.add(colorNuevo);
+	document.querySelector(".iris-izq").classList.remove(colorActual);
+	document.querySelector(".iris-der").classList.remove(colorActual);
+	document.querySelector(".iris-izq").classList.add(colorNuevo);
+	document.querySelector(".iris-der").classList.add(colorNuevo);
 }
 export function Avatar() {
-    return (
-		
+	return (
+
 		<div className="componente-avatar">
 			<div className="componente-avatar-modificacion">
 				<div className="componente-principal">
@@ -51,12 +56,12 @@ export function Avatar() {
 						<div className="oreja-izq piel-default"></div>
 						<div className="cabeza piel-default">
 							<div className="contendor-pelo">
-								<img id="pelo-actual" src={pelo}/>
+								<img id="pelo-actual" src={pelo} />
 							</div>
 							<div className="cejas-ojos-nariz-boca">
 								<div className="contenedor-cejas">
-									<div className="ceja-izq"></div>
-									<div className="ceja-der"></div>
+									<div className="ceja-izq ceja-negra"></div>
+									<div className="ceja-der ceja-negra"></div>
 								</div>
 								<div className="contendor-ojos">
 									<div className="ojo-izq">
@@ -78,7 +83,7 @@ export function Avatar() {
 					</div>
 					<div className="cuello piel-default"></div>
 					<div className="componente-ropa">
-							<img src={ropa}/>
+						<img src={ropa} />
 					</div>
 					<div className="contendor-brazos">
 						<div className="brazo-izq piel-default"></div>
@@ -86,68 +91,68 @@ export function Avatar() {
 					</div>
 				</div>
 
-				
+
 				<div className="componente-cambio-aspecto">
 					<div className="modificar color-ojo">
 						<div className="ojo ojo-marron"
-							 onClick={()=>setOjos("iris-marron")}>
-								<div className="iris iris-marron">
-									<div className="pupila" title="ojos marrones"></div>
-								</div>
+							onClick={() => setOjos("iris-marron")}>
+							<div className="iris iris-marron">
+								<div className="pupila" title="ojos marrones"></div>
 							</div>
+						</div>
 
 						<div className="ojo ojo-verde"
-							 onClick={()=>setOjos("iris-verde")}>
-								<div className="iris iris-verde">
-									<div className="pupila" title="ojos verdes"></div>
-								</div>
+							onClick={() => setOjos("iris-verde")}>
+							<div className="iris iris-verde">
+								<div className="pupila" title="ojos verdes"></div>
+							</div>
 						</div>
 						<div className="ojo ojo-celeste"
-							 onClick={()=>setOjos("iris-celeste")}>
-								<div className="iris iris-celeste">
-									<div className="pupila" title="ojos celestes"></div>
-								</div>
+							onClick={() => setOjos("iris-celeste")}>
+							<div className="iris iris-celeste">
+								<div className="pupila" title="ojos celestes"></div>
+							</div>
 						</div>
 					</div>
 					<div className="modificar color-piel">
 						<div className="piel piel-default"
-							 title="default"
-							 onClick={()=>setPiel("piel-default")}></div>
+							title="default"
+							onClick={() => setPiel("piel-default")}></div>
 						<div className="piel piel-rosa"
-							 title="piel rosa"
-							 onClick={()=>setPiel("piel-rosa")}></div>
+							title="piel rosa"
+							onClick={() => setPiel("piel-rosa")}></div>
 						<div className="piel piel-morada"
-							 title="piel morada"
-							 onClick={()=>setPiel("piel-morada")}></div>
+							title="piel morada"
+							onClick={() => setPiel("piel-morada")}></div>
 						<div className="piel piel-marron"
-							 title="piel marron"
-							 onClick={()=>setPiel("piel-marron")}></div>
+							title="piel marron"
+							onClick={() => setPiel("piel-marron")}></div>
 						<div className="piel piel-oscura"
-							 title="piel oscura"
-							 onClick={()=>setPiel("piel-oscura")}></div>
+							title="piel oscura"
+							onClick={() => setPiel("piel-oscura")}></div>
 					</div>
 					<div className="modificar color-pelo">
-						<div className="pelo pelo-negro" 
-							 title="pelo negro"
-							 onClick={()=>setPelo(pelo)}></div>
+						<div className="pelo pelo-negro"
+							title="pelo negro"
+							onClick={() => setPelo(pelo, "ceja-negra")}></div>
 						<div className="pelo pelo-castano"
-							 title="pelo castaño"
-							 onClick={()=>setPelo(peloCastano)}></div>
-						<div className="pelo pelo-rubio" 
-							 title="pelo rubio"
-							 onClick={()=>setPelo(peloRubio)}></div>
-						<div className="pelo pelo-colorado" 
-							 title="pelo colorado"
-							 onClick={()=>setPelo(peloColorado)}></div>
-						<div className="pelo pelo-canoso" 
-							 title="pelo canoso"
-							 onClick={()=>setPelo(peloCanoso)}></div>
-					</div>	
+							title="pelo castaño"
+							onClick={() => setPelo(peloCastano, "ceja-castana")}></div>
+						<div className="pelo pelo-rubio"
+							title="pelo rubio"
+							onClick={() => setPelo(peloRubio, "ceja-rubia")}></div>
+						<div className="pelo pelo-colorado"
+							title="pelo colorado"
+							onClick={() => setPelo(peloColorado, "ceja-colorada")}></div>
+						<div className="pelo pelo-canoso"
+							title="pelo canoso"
+							onClick={() => setPelo(peloCanoso, "ceja-canosa")}></div>
+					</div>
 				</div>
 			</div>
 		</div>
-		
-				
-        
-    );
+
+
+
+	);
 }
