2ba72d7aa99ddbdc70c2609d194851afb58fb5a6
julieta96 <julietaextra14@gmail.com>
2022-09-19 12:41:44 -0300

    update avatar-view

--- TrucoApp-DevCode/ClientApp/src/components/avatar/Avatar.js
+++ TrucoApp-DevCode/ClientApp/src/components/avatar/Avatar.js
@@ -50,6 +50,7 @@ export function Avatar() {
 	return (
 
 		<div className="componente-avatar">
+			<h1 className="titulo">Crea tu avatar y personalizalo</h1>
 			<div className="componente-avatar-modificacion">
 				<div className="componente-principal">
 					<div className="avatar">
@@ -93,6 +94,7 @@ export function Avatar() {
 
 
 				<div className="componente-cambio-aspecto">
+				    <strong style={{fontSize:"18px"}}>Color de ojos</strong>
 					<div className="modificar color-ojo">
 						<div className="ojo ojo-marron"
 							onClick={() => setOjos("iris-marron")}>
@@ -114,39 +116,51 @@ export function Avatar() {
 							</div>
 						</div>
 					</div>
+					<strong style={{fontSize:"18px"}}>Color de piel</strong>
 					<div className="modificar color-piel">
 						<div className="piel piel-default"
 							title="default"
-							onClick={() => setPiel("piel-default")}></div>
+							onClick={() => setPiel("piel-default")}>
+						</div>
 						<div className="piel piel-rosa"
 							title="piel rosa"
-							onClick={() => setPiel("piel-rosa")}></div>
+							onClick={() => setPiel("piel-rosa")}>
+						</div>
 						<div className="piel piel-morada"
 							title="piel morada"
-							onClick={() => setPiel("piel-morada")}></div>
-						<div className="piel piel-marron"
-							title="piel marron"
-							onClick={() => setPiel("piel-marron")}></div>
+							onClick={() => setPiel("piel-morada")}>
+						</div>
+						<div className="piel piel-morena"
+							title="piel morena"
+							onClick={() => setPiel("piel-morena")}>
+						</div>
 						<div className="piel piel-oscura"
 							title="piel oscura"
-							onClick={() => setPiel("piel-oscura")}></div>
+							onClick={() => setPiel("piel-oscura")}>		
+						</div>
 					</div>
+					<strong style={{fontSize:"18px"}}>Color de pelo</strong>
 					<div className="modificar color-pelo">
 						<div className="pelo pelo-negro"
 							title="pelo negro"
-							onClick={() => setPelo(pelo, "ceja-negra")}></div>
+							onClick={() => setPelo(pelo, "ceja-negra")}>
+						</div>
 						<div className="pelo pelo-castano"
 							title="pelo castaño"
-							onClick={() => setPelo(peloCastano, "ceja-castana")}></div>
+							onClick={() => setPelo(peloCastano, "ceja-castana")}>
+						</div>
 						<div className="pelo pelo-rubio"
 							title="pelo rubio"
-							onClick={() => setPelo(peloRubio, "ceja-rubia")}></div>
+							onClick={() => setPelo(peloRubio, "ceja-rubia")}>
+						</div>
 						<div className="pelo pelo-colorado"
 							title="pelo colorado"
-							onClick={() => setPelo(peloColorado, "ceja-colorada")}></div>
+							onClick={() => setPelo(peloColorado, "ceja-colorada")}>	
+					    </div>
 						<div className="pelo pelo-canoso"
 							title="pelo canoso"
-							onClick={() => setPelo(peloCanoso, "ceja-canosa")}></div>
+							onClick={() => setPelo(peloCanoso, "ceja-canosa")}>
+						</div>
 					</div>
 				</div>
 			</div>
