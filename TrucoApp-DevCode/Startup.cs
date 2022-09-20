b2aa43531b7dad2f50f0b81b2f938d36ddcbbf40
Ivan Kek <ivan.kek@hotmail.com>
2022-09-20 00:16:14 -0300

    Agregando conexión proyecto con la base

--- TrucoApp-DevCode/Startup.cs
+++ TrucoApp-DevCode/Startup.cs
@@ -1,7 +1,9 @@
+using Entidades;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,9 @@ namespace Router
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddDbContext<DevCodeDBContext>(options => options.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("DevCodeDBContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
