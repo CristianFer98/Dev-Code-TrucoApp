6abc2019e21d469f6510bd510eb4cc73cb4acdf8
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-09-21 20:36:42 -0300

    upd

--- TrucoApp-DevCode/ClientApp/src/components/nosotros/Nosotros.js
+++ TrucoApp-DevCode/ClientApp/src/components/nosotros/Nosotros.js
@@ -1,9 +1,137 @@
 import React from 'react';
+import './nosotros.css';
+import perfil from './../../assets/no-foto.jpg';
+import students from './../../assets/students.jpg';
 
 export function Nosotros() {
   return (
     <>
-      <h1 style={{ color: 'black' }}> Pagina Nosotros </h1>
+      <div className="nosotros-container container-fluid text-center">
+        <div className="row">
+          <div className="col">
+            <div
+              id="carouselExampleSlidesOnly"
+              className="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    id="img-students"
+                    src={students}
+                    className="d-block w-100"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-6 col-xl-4">
+            <div className="card m-5 p-2">
+              <img
+                src={perfil}
+                className="card-img-top"
+                alt="Tarjeta de integrante"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button className="btn btn-primary">LinkedIn</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-4">
+            <div className="card m-5 p-2">
+              <img
+                src={perfil}
+                className="card-img-top"
+                alt="Tarjeta de integrante"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button className="btn btn-primary">LinkedIn</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-4">
+            <div className="card m-5 p-2">
+              <img
+                src={perfil}
+                className="card-img-top"
+                alt="Tarjeta de integrante"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button className="btn btn-primary">LinkedIn</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-4">
+            <div className="card m-5 p-2">
+              <img
+                src={perfil}
+                className="card-img-top"
+                alt="Tarjeta de integrante"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button className="btn btn-primary">LinkedIn</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-4">
+            <div className="card m-5 p-2">
+              <img
+                src={perfil}
+                className="card-img-top"
+                alt="Tarjeta de integrante"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button className="btn btn-primary">LinkedIn</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-xl-4">
+            <div className="card m-5 p-2">
+              <img
+                src={perfil}
+                className="card-img-top"
+                alt="Tarjeta de integrante"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button className="btn btn-primary">LinkedIn</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
