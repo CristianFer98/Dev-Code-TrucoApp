9f24bbbdd36e0566f87039ffb86140bbe9bea74b
julieta96 <julietaextra14@gmail.com>
2022-09-29 20:21:36 -0300

    tienda avatar

---
+++ TrucoApp-DevCode/ClientApp/src/components/tienda/TiendaAvatar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './tienda.css';
+import ropaV2 from './../../assets/avatar/ropa-v2.png';
+import ropaV3 from './../../assets/avatar/ropa-v3.png';
+import ropaV4 from './../../assets/avatar/ropa-v4.png';
+import ropaV5 from './../../assets/avatar/ropa-v5.png';
+export function TiendaAvatar() {
+ 
+  return (
+    <div className="componente-store">
+      <h1 className="titulo mb-5">Lookea tu Avatar con el mejor estilo </h1>
+      
+      <div className="card p-3" style={{width:'80%'}}>
+            <div className="card-header">
+              <h3 className="card-title text-dark">Ropa</h3>
+            </div>
+            <div className="card- d-flex flex-row">
+                <div className="card">
+                    <img className="card-img-top" src={ropaV2} alt="Card image cap"/>
+                    <div className="card-body">
+                       <p className="card-text d-flex flex-column text-center">
+                            <strong> $300</strong>
+                            <span class="badge bg-danger p-3" style={{cursor:'pointer'}}>COMPRAR</span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="card" >
+                    <img className="card-img-top" src={ropaV3} alt="Card image cap"/>
+                    <div className="card-body">
+                       <p className="card-text d-flex flex-column text-center">
+                            <strong> $300</strong>
+                            <span class="badge bg-danger p-3" style={{cursor:'pointer'}}>COMPRAR</span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img className="card-img-top" src={ropaV4} alt="Card image cap"/>
+                    <div className="card-body">
+                        <p className="card-text d-flex flex-column text-center">
+                            <strong> $300</strong>
+                            <span class="badge bg-danger p-3" style={{cursor:'pointer'}}>COMPRAR</span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img className="card-img-top" src={ropaV5} alt="Card image cap"/>
+                    <div className="card-body">
+                        <p className="card-text d-flex flex-column text-center">
+                            <strong> $300</strong>
+                            <span class="badge bg-danger p-3" style={{cursor:'pointer'}}>COMPRAR</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <a href="#" className="btn btn-danger mt-3">COMPRAR TODO</a>
+      </div> 
+     
+    </div>
+  );
+}
