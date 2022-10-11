34ad6d478ba636bc840147e5a11349ee1fb97564
julieta96 <julietaextra14@gmail.com>
2022-10-11 09:22:47 -0300

    avatar repositorio

---
+++ Repositorios/AvatarRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using Entidades;
+using Repositorios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios
+{
+    public class AvatarRepositorio:IAvatarRepositorio
+    {
+        private readonly DevCodeDBContext _dbContext;
+
+        public AvatarRepositorio(DevCodeDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void GuardarAvatar(Avatar avatar)
+        {
+            
+                 _dbContext.Avatars.Add(avatar);
+                 _dbContext.SaveChanges();
+             
+            //throw new NotImplementedException();
+        }
+
+        public Avatar MostrarAvatarPorUsuario(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModificarAvatar(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
