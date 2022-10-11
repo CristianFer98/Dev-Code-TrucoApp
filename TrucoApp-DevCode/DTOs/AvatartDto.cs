cb51fb5df56596f1428f1147108339731a9aa5c2
julieta96 <julietaextra14@gmail.com>
2022-10-11 09:24:46 -0300

    avatar dtos

---
+++ TrucoApp-DevCode/DTOs/AvatartDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DTOs
+{
+    public class AvatartDto
+    {
+        public int IdUsuario { get; set; }
+        public string Pelo { get; set; }
+        public string Ceja { get; set; }
+        public string ColorDePiel { get; set; }
+        public string ColorDeOjos { get; set; }
+        public string Ropa { get; set; }
+    }
+}
