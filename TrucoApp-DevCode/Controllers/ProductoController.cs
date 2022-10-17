d783c91936f225fc7ecbc7ba88d9b07c86949fde
julieta96 <julietaextra14@gmail.com>
2022-10-17 20:00:13 -0300

    producto controller

---
+++ TrucoApp-DevCode/Controllers/ProductoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.Interfaces;
+using System;
+
+namespace Router.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly IProductoServicio _productoServicio;
+
+        public ProductoController(IProductoServicio productoServicio)
+        {
+            _productoServicio = productoServicio;
+        }
+
+        [HttpGet]
+        [Route("ObtenerProductos")]
+        public ActionResult ObtenerProductos()
+        {
+            try
+            {
+
+                return StatusCode(StatusCodes.Status200OK, _productoServicio.GetProductos());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
