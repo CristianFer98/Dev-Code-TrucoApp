b0a3f7a8b8f74d562ea82f19c07c14e52ed52912
julieta96 <julietaextra14@gmail.com>
2022-10-17 19:58:26 -0300

    servicio producto

---
+++ Servicios/ProductoServicio.cs
@@ -0,0 +1,36 @@
+﻿using Entidades;
+using Repositorios.Interfaces;
+using Servicios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class ProductoServicio:IProductoServicio
+    {
+        private readonly IProductoRepositorio _productoRepositorio;
+
+        public ProductoServicio(IProductoRepositorio productoRepositorio)
+        {
+            _productoRepositorio = productoRepositorio;
+        }
+
+        public void ComprarProducto(int idProducto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Producto GetProductoPorId(int idProducto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Producto> GetProductos()
+        {
+            return _productoRepositorio.GetProductos();
+        }
+    }
+}
