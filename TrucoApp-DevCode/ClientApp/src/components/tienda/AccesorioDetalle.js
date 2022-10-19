fc2b64119bdc2b663da1e34c080a764c4c877ea3
julieta96 <julietaextra14@gmail.com>
2022-10-19 11:21:08 -0300

    se trae los detalles del producto desde la bd

--- TrucoApp-DevCode/ClientApp/src/components/tienda/AccesorioDetalle.js
+++ TrucoApp-DevCode/ClientApp/src/components/tienda/AccesorioDetalle.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import './accesorios.css';
+import imagenes from './TiendaImagenes';
 import { Link } from 'react-router-dom';
-const AccesorioDetalle = ({ imagen, descripcion, detalle1, detalle2, detalle3, precio }) => {
+const AccesorioDetalle = ({ imagen, descripcion, cantidadAComprar, medidas, marca,tipoBaraja, precio }) => {
   return (
     <><h1 className="text-center mt-5 mb-4">{descripcion}</h1>
     <div className="card p-4 border-0" style={{ width: '50rem' }}>
        <div className="d-flex flex-row justify-content-center">
             <div className="accesorio-componente">
                 <div className="accesorio-imagen p-5" style={{ background: '#d9d9d9'}}>
-                    <img className="card-img-top" src={imagen} alt={descripcion} style={{ width:'400px', height:'400px' }}/>
+                    <img className="card-img-top" src={imagenes[imagen]} alt={descripcion} style={{ width:'400px', height:'400px' }}/>
                 </div>
             </div>
             <div className="detalles d-flex flex-row ms-5">
 
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item mb-2">{detalle1}</li>
-                    <li className="list-group-item mb-2">{detalle2}</li>
-                    <li className="list-group-item mb-2">{detalle3}</li>
+                    <li className="list-group-item mb-2"  style={ medidas==null ? { display:'none'} : {display : 'block'} }>
+                      <strong>Medidas: </strong> {medidas}
+                    </li>
+                    <li className="list-group-item mb-2"  style={ marca==null ? { display:'none'} : {display : 'block'} }>
+                      <strong>Marca: </strong> {marca}
+                    </li>
+                    <li className="list-group-item mb-2"  style={ tipoBaraja==null ? { display:'none'} : {display : 'block'} }>
+                      <strong>Tipo de Baraja: </strong> {tipoBaraja}
+                    </li>
+                    <li className="list-group-item mb-2">
+                      <strong>Cantidad a Comprar: </strong> {cantidadAComprar}
+                    </li>
                     <li className="list-group-item mb-4">
                         Precio: <strong>${precio}</strong>
                     </li>
