e5c921b458ccdbb138b43eb804141b6a60e1fd97
julieta96 <julietaextra14@gmail.com>
2022-10-20 18:52:56 -0300

    controller accesorio

---
+++ TrucoApp-DevCode/Controllers/AccesorioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.Interfaces;
+using System;
+
+namespace Router.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccesorioController : ControllerBase
+    {
+        private readonly IAccesorioServicio _accesorioServicio;
+        public AccesorioController(IAccesorioServicio accesorioServicio)
+        {
+            _accesorioServicio = accesorioServicio;
+        }
+
+        [HttpGet]
+        [Route("ObtenerAccesorios")]
+        public ActionResult ObtenerAccesorios()
+        {
+            try
+            {
+
+                return StatusCode(StatusCodes.Status200OK, _accesorioServicio.GetAccesorios());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
