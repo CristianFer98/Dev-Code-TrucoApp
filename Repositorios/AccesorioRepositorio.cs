cfba1a6413d278bf05f352d7f581cd34b0ff6a2d
julieta96 <julietaextra14@gmail.com>
2022-10-20 18:52:07 -0300

    repositorio accesorio

---
+++ Repositorios/AccesorioRepositorio.cs
@@ -0,0 +1,25 @@
+﻿using Entidades;
+using Repositorios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios
+{
+    public class AccesorioRepositorio:IAccesorioRepositorio
+    {
+        private readonly DevCodeDBContext _dbContext;
+
+        public AccesorioRepositorio(DevCodeDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Accesorio> GetAccesorios()
+        {
+            return _dbContext.Accesorios.ToList();
+        }
+    }
+}
