56ce8779f677d01284d6c6ce7ac1f1eb74e424a5
julieta96 <julietaextra14@gmail.com>
2022-10-20 18:52:30 -0300

    servicio accesorio

---
+++ Servicios/Interfaces/IAccesorioServicio.cs
@@ -0,0 +1,14 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Interfaces
+{
+    public interface IAccesorioServicio
+    {
+        public List<Accesorio> GetAccesorios();
+    }
+}
