a04e215fd950703faa6580a0502890b1a8aee1ba
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-11-14 11:46:04 -0300

    Create MainController.cs
    Parar poder crear db desde endpoint api

---
+++ TrucoApp-DevCode/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Router.Controllers
+{
+[Route("api/[controller]")]
+[ApiController]
+public class MainController : ControllerBase
+{
+    Entidades.DevCodeDBContext dbcontext;
+
+    public MainController(DevCodeDBContext dbcontext)
+    {
+        this.dbcontext = dbcontext;
+    }
+
+        [HttpGet]
+        [Route("createdb")]
+        public IActionResult CreateDatabase()
+        {
+            dbcontext.Database.EnsureCreated();
+
+            return Ok();
+        }
+    }
+}
