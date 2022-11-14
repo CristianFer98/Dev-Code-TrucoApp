9af2466b68056b52a5c5adf386bb6f8b27bb503d
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-11-14 13:20:22 -0300

    TorneoController + config

---
+++ TrucoApp-DevCode/Controllers/TorneoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Servicios.Interfaces;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Router.Hubs;
+using Servicios;
+
+namespace Router.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TorneoController : ControllerBase
+    {
+        ITorneoServicio _torneoServicio;
+
+        public TorneoController(ITorneoServicio torneoServicio)
+        {
+            _torneoServicio = torneoServicio;
+        }
+
+        [HttpGet]
+        [Route("ObtenerTodosLosTorneos")]
+        public IActionResult Get()
+        {
+            return Ok(_torneoServicio.ObtenerTorneosDisponibles());
+        }
+    }
+}
