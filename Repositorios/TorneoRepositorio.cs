636254ebece67d90f911c68ea536972405241796
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-11-14 13:25:24 -0300

    Squashed commit of the following:
    commit 9af2466b68056b52a5c5adf386bb6f8b27bb503d
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Mon Nov 14 13:20:22 2022 -0300
    
        TorneoController + config
    
    commit 3318739e8e73f44ea126adacd4e74cb01be5e560
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Mon Nov 14 13:20:00 2022 -0300
    
        Torneo Servicio
    
    commit b87ac5bed09bbdb9573c60f70105230651959879
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Mon Nov 14 13:19:40 2022 -0300
    
        Torneo Repositorio
    
        con metodo ObtenerTorneosDisponibles
    
    commit 6e7f37b2f842d0eb0fadc431f586ce906b9f29aa
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Mon Nov 14 12:42:44 2022 -0300
    
        Update entidad Torneo
    
        Se añade propiedad Terminado
    
    commit a04e215fd950703faa6580a0502890b1a8aee1ba
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Mon Nov 14 11:46:04 2022 -0300
    
        Create MainController.cs
    
        Parar poder crear db desde endpoint api
    
    commit 260aab757718cb12792d7bf52d13ce2afd0ef479
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Mon Nov 14 11:45:37 2022 -0300
    
        Entidad Torneos

---
+++ Repositorios/TorneoRepositorio.cs
@@ -0,0 +1,26 @@
+﻿using Entidades;
+using Repositorios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Repositorios
+{
+    public class TorneoRepositorio : ITorneoRepositorio
+    {
+        DevCodeDBContext _dbContext;
+        
+        public TorneoRepositorio(DevCodeDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Torneo> ObtenerTorneosDisponibles()
+        {
+            return _dbContext.Torneos.Where(t=> t.Terminado == false);
+        }
+    }
+}
---
+++ Servicios/Interfaces/ITorneoServicio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Servicios.Interfaces
+{
+    public interface ITorneoServicio
+    {
+        IEnumerable<Torneo> ObtenerTorneosDisponibles();
+    }
+}
