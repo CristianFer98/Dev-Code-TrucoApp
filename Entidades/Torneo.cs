835233ab1c7b28b123f26b29dfa94e51c025f14f
Ignacio Damián González <ignaciodamiang@gmail.com>
2022-11-21 18:55:57 -0300

    Squashed commit of the following:
    commit 7f7a6eeee13be213a157bfcc21c386d5f9431808
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Mon Nov 21 18:53:50 2022 -0300
    
        Pasar de ronda en torneo
    
    commit 2f3d4418fdd59f2b8898537d57a8cf61d5568e97
    Merge: 1b62bc6 f92ce5f
    Author: Ignacio Damián González <ignaciodamiang@gmail.com>
    Date:   Sun Nov 20 11:47:34 2022 -0300
    
        Merge branch 'Feature_Torneo' into torneos_2
    
    commit f92ce5f6ba638f024f3d8e0ea8a6388fb2c82a84
    Author: Nahuel Giorgi <nahuel@remitee.com>
    Date:   Thu Nov 17 16:42:13 2022 -0300
    
        Creacion de torneo

--- Entidades/Torneo.cs
+++ Entidades/Torneo.cs
@@ -1,18 +1,20 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Entidades
 {
     public class Torneo
     {
-        public Guid IdTorneo { get; set; }
+        public int IdTorneo { get; set; }
         public string Nombre { get; set; }
-        public int EtapaTorneo { get; set; }
-        public bool HabilitadoJugar { get; set; }
-        public bool Terminado { get; set; }
+        public int CantidadParticipantes { get; set; }
+        public bool Terminado { get; set; } = false;
+        public int nroRonda { get; set; } = 0;
+        public virtual ICollection<TorneoPartida> Partidas { get; set; }
+        public virtual ICollection<TorneoParticipante> Participantes { get; set; }
+
+        public bool EstaLleno()
+        {
+            return Participantes.Count >= CantidadParticipantes;
+        }
     }
 }
--- Repositorios/Interfaces/ITorneoRepositorio.cs
+++ Repositorios/Interfaces/ITorneoRepositorio.cs
@@ -1,15 +1,13 @@
 ﻿using Entidades;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositorios.Interfaces
 {
     public interface ITorneoRepositorio
     {
+        Torneo ObtenerPorId(int idTorneo);
         IEnumerable<Torneo> ObtenerTorneosDisponibles();
         Torneo CrearTorneo(Torneo torneo);
+        void SetearRondas(int idTorneo, int ronda);
     }
 }
