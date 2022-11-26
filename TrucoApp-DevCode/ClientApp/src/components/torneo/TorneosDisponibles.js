b1850cd107b6d358d2409f2e0991b1106f657f98
Lucas Benitez <lucasbenitez323@gmail.com>
2022-11-25 21:44:31 -0300

    create torneoDisponible.js

---
+++ TrucoApp-DevCode/ClientApp/src/components/torneo/TorneosDisponibles.js
@@ -0,0 +1,62 @@
+﻿import React from "react";
+import { useHistory } from "react-router";
+import { BotonCrearTorneo } from ".//BotonCrearTorneo";
+import { TorneoDisponibleCard } from "./TorneoDisponibleCard";
+import { ChatGeneral } from "../inicio/chat/ChatGeneral";
+import InfoDeUsuario from "../inicio/infoUsuario/InfoDeUsuario";
+import Button from "react-bootstrap/Button";
+import { useSelector } from "react-redux";
+
+export const TorneosDisponibles = () => {
+    const history = useHistory();
+    const { torneosPartida } = useSelector((state) => state.torneos);
+    console.log(torneosPartida)
+
+    const handleVolverInicio = async (e) => {
+        e.preventDefault();
+        history.push("/inicio");
+    };
+
+    return (
+        <div style={{ display: "flex", width: "100%" }}>
+            <div style={{ display: "flex", width: "80%", flexDirection: "column" }}>
+                <InfoDeUsuario />
+                <div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginTop: "20px",
+                        }}
+                    >
+                        <Button
+                            variant="dark"
+                            onClick={handleVolverInicio}
+                            style={{ marginLeft: "50px" }}
+                        >
+                            Regresar al Inicio
+                        </Button>{" "}
+                    </div>
+                    <div
+                        className="d-flex flex-wrap mt-4"
+                        style={{
+                            marginLeft: "40px",
+                            marginRight: "40px",
+                            marginTop: "0px",
+                        }}
+                    >
+
+                            <BotonCrearTorneo />    
+                        {torneosPartida.map((torneo) => (
+                            <TorneoDisponibleCard key={torneo.idTorneo} torneo={torneo} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+            <div className="chat">
+                <ChatGeneral />
+            </div>
+        </div>
+    );
+};
